53ecb3405c685e4de92d26338f22610b49025c78
chaitanya722 <chaitanyapawar410@gmail.com>
2025-05-02 17:59:41 +0530

    :done:

--- backend/routes/investment.js
+++ backend/routes/investment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Add Investment API
 router.post("/:userId", async (req, res) => {
--- backend/routes/notificationRoute.js
+++ backend/routes/notificationRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const UserNotification = require('../models/notification');
 const transporter = require('../config/mailConfig');
 require('dotenv').config();
